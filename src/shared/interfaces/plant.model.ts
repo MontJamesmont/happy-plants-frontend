eb20172c53c17b4355080760ed4b53e869638b51
Stefan <sejsmont@creativeadvtech.com>
2026-09-01 14:31:49 +0200

    plants watering, getLoggedUser after plants update, patchname endpoint

--- src/shared/interfaces/plant.model.ts
+++ src/shared/interfaces/plant.model.ts
@@ -5,6 +5,7 @@ export interface Plant {
   ownersPlantName?: string; // user's custom name for the plant
   commonName?: string; // common name from Perenual
   firstWateringDay?: string; // ISO date string set when plant is added (last watered date)
-  wateringIntervalDays?: number; // benchmark interval (from Perenual) in days
+  wateringIntervalDaysValue?: number; // benchmark interval (from Perenual)
+  wateringIntervalDaysUnit?: string; // unit for the watering interval
   images?: string[]; // optional image URLs
 }
